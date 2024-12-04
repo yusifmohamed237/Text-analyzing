9bca9ee57b4e72ec6e675b7bb0948f419ac02a6c
hazemgawwad <169104541+hazemgawwad@users.noreply.github.com>
2024-12-04 18:45:34 +0200

    add file upload function

---
+++ FileUpload.fs
@@ -0,0 +1,13 @@
+﻿module FileUpload
+
+open System
+open System.IO
+
+let loadTextFromFile (filePath: string) =
+    try
+        File.ReadAllText(filePath)
+    with
+    | :? FileNotFoundException -> "File not found."
+    | ex -> sprintf "An error occurred: %s" ex.Message
+
+
