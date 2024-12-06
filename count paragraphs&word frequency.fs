f3596b291509e9b001ff16817d88de59c5cf17f0
hagerSaad1 <131702291+hagerSaad1@users.noreply.github.com>
2024-12-06 18:53:54 +0200

    Add files via upload

---
+++ count paragraphs&word frequency.fs
@@ -0,0 +1,17 @@
+﻿// Function to count paragraphs (assuming paragraphs are separated by double newlines)
+let countParagraphs (text: string) =
+    let paragraphs = text.Split([| "\n\n" |], StringSplitOptions.RemoveEmptyEntries)
+    paragraphs.Length
+
+// Function to count word frequency
+let wordFrequency (text: string) =
+    let words = text.Split([|' '; '\t'; '\n'; '\r'; '.'; ','; ';'; ':'; '!'|], StringSplitOptions.RemoveEmptyEntries)
+    words
+    |> Array.fold (fun acc word ->
+        let word = word.ToLower()
+        if acc.ContainsKey(word) then
+            acc.[word] <- acc.[word] + 1
+        else
+            acc.Add(word, 1)
+        acc
+    ) (System.Collections.Generic.Dictionary<string, int>())
